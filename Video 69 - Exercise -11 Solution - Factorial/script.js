202b87ae896d1e03453c42903e00038303b156cd
rahulbhatt-dev <rahulbhatt.developer@gmail.com>
2025-10-08 15:06:56 +0530

    commit

--- Video 69 - Exercise -11 Solution - Factorial/script.js
+++ Video 69 - Exercise -11 Solution - Factorial/script.js
@@ -1,7 +1,9 @@
 let a = 7
 
 function factorial(number){
+    //storing array keys (0,1,2.....number +1)
    let arr = Array.from(Array(number+1).keys())
+   //we started from 1 and not zero for facotrial and then reduce
    let c = arr.slice(1,).reduce((a, b)=> a*b )
    return c
 }
