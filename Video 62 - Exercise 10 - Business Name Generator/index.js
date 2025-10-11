6680b681fd3f2b207db7ab9fe06fb090a6920572
rahulbhatt-dev <rahulbhatt.developer@gmail.com>
2025-10-11 18:27:46 +0530

    Update index.js
    Solution

--- Video 62 - Exercise 10 - Business Name Generator/index.js
+++ Video 62 - Exercise 10 - Business Name Generator/index.js
@@ -19,5 +19,28 @@ Hub
 
 
 */
+let Adjectives = {
+    1:"Crazy",
+    2:"Amazing",
+    3:"Fire"
+}
 
+let Shop = {
+    1:"Engine",
+    2:"Foods",
+    3:"Garments"
+}
 
+let Another = {
+    1:"Bros",
+    2:"Limited",
+    3:"Hub"
+}
+
+let var1 = Math.floor((Math.random() *(3-1 +1 ) )+1);
+let var2 = Math.floor((Math.random() *(3-1 +1 ) )+1);
+let var3 = Math.floor((Math.random() *(3-1 +1 ) )+1);
+
+console.log(var1 + " "+var2+ " "+var3);
+
+console.log(Adjectives[var1] + " "+Shop[var2]+ " "+Another[var3]);
